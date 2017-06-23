9119b3add38ab85d4b33d5b5b937df0076c96205
KoretsValentyn <KoretsValentyn@gmail.com>
2017-06-23 16:25:25 +0300

    all project

---
+++ TPP_Main_Repository-master/TPP_MainProject/Controllers/OrderOperatorController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TPP_MainProject.Models.entities;
+using TPP_MainProject.Models;
+using TPP_MainProject.Models.repository;
+using TPP_MainProject.Models.constants;
+
+namespace TPP_MainProject.Controllers
+{
+    public class OrderOperatorController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        private UnitOfWork unitOfWork = new UnitOfWork();
+        private IEnumerable<Order> activeOrders;
+
+        // GET: /OrderOperator/
+        public ActionResult Index()
+        {
+            activeOrders  = unitOfWork.OrderRepository.Get().Where(s => s.orderStartus.Equals(OrderStatus.Initiating));
+            return View(activeOrders);
+        }
+
+
+
+        public ActionResult Reject(int? id)
+        {
+            Order ord = unitOfWork.OrderRepository.GetByID(id);
+            ord.orderStartus = OrderStatus.Rejected;
+            unitOfWork.OrderRepository.Update(ord);
+            unitOfWork.Save();
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Confrim(int? id)
+        {
+
+            IEnumerable<ApplicationUser> them = unitOfWork.UserRepository.Get().Where(s => s.RoleName.Equals(RolesConst.MANAGER));
+
+            ViewBag.pm = them;
+            //make enumerable from enum
+            //ViewBag.ps = (IEnumerable<ProjectStatus>)Enum.GetValues(typeof(ProjectStatus)); 
+            Project proj = new Project();
+           
+            return View(proj);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Confrim(Project pro)
+        {
+            if (ModelState.IsValid)
+            {
+                Order ord = unitOfWork.OrderRepository.GetByID(pro.id);
+
+                ord.orderStartus = OrderStatus.Processiong;
+                unitOfWork.OrderRepository.Update(ord);
+                
+             
+                pro.order = ord;
+                pro.costs = ord.Total;
+                IEnumerable<ApplicationUser> them =  unitOfWork.UserRepository.Get().Where(s => s.RoleName.Equals(RolesConst.MANAGER));
+                foreach (ApplicationUser manager in them)
+                {
+                    if (manager.UserName.Equals(pro.nameProjectManager))
+                        pro.projectManager = manager;
+                }
+              
+                pro.projectStatus = ProjectStatus.Initial;
+
+                unitOfWork.ProjectRepository.Insert(pro);
+                unitOfWork.Save();
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
